c40c024711b730225a6c524f115c3ed906890ec0
James Armes <jarmes@codeforamerica.org>
2025-05-09 10:48:03 -0400

    fix: Output for `repository_url` when not creating a repository.

--- outputs.tf
+++ outputs.tf
@@ -8,8 +8,8 @@ output "docker_push" {
   value       = !var.create_repository ? "" : <<EOT
 aws ecr get-login-password --region ${data.aws_region.current.name} | docker login --username AWS --password-stdin ${module.ecr["this"].repository_registry_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com
 docker build -t ${module.ecr["this"].repository_name} --platform linux/amd64 .
-docker tag ${module.ecr["this"].repository_name}:${var.image_tag} ${module.ecr["this"].repository_url}:latest
-docker push ${module.ecr["this"].repository_url}:latest
+docker tag ${module.ecr["this"].repository_name}:${var.image_tag} ${local.image_url}:latest
+docker push ${local.image_url}:latest
 EOT
 }
 
@@ -20,7 +20,7 @@ output "repository_arn" {
 
 output "repository_url" {
   description = "URL of the container image repository."
-  value       = module.ecr["this"].repository_url
+  value       = local.image_url
 }
 
 output "version_parameter" {
