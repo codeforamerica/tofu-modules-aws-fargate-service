445f37601053e9aa4710bb27be235e25b4aa23b9
James Armes <jarmes@codeforamerica.org>
2026-01-22 14:32:14 -0500

    fix: Circular reference.

--- kms.tf
+++ kms.tf
@@ -4,10 +4,10 @@ resource "aws_kms_key" "fargate" {
   enable_key_rotation     = true
   policy = jsonencode(yamldecode(templatefile("${path.module}/templates/key-policy.yaml.tftpl", {
     account_id : data.aws_caller_identity.identity.account_id
-    ecr_arn : local.repository_arn
     ecs_roles : [aws_iam_role.execution.arn, aws_iam_role.task.arn]
     partition : data.aws_partition.current.partition
     region : data.aws_region.current.name
+    repository_name : local.prefix
   })))
 
   tags = var.tags
