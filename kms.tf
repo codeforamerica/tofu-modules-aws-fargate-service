19850a299da4eba2d04dcd8b545fcb279ebb919a
James Armes <jarmes@codeforamerica.org>
2026-01-22 12:23:48 -0500

    fix: Update KMS key policy to prevent public access.

--- kms.tf
+++ kms.tf
@@ -3,10 +3,11 @@ resource "aws_kms_key" "fargate" {
   deletion_window_in_days = var.key_recovery_period
   enable_key_rotation     = true
   policy = jsonencode(yamldecode(templatefile("${path.module}/templates/key-policy.yaml.tftpl", {
-    account_id : data.aws_caller_identity.identity.account_id,
-    partition : data.aws_partition.current.partition,
-    region : data.aws_region.current.name,
-    repository_name : local.prefix,
+    account_id : data.aws_caller_identity.identity.account_id
+    ecr_arn : local.repository_arn
+    ecs_roles : [aws_iam_role.execution.arn, aws_iam_role.task.arn]
+    partition : data.aws_partition.current.partition
+    region : data.aws_region.current.name
   })))
 
   tags = var.tags
