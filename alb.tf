bdf8391a8ae7773d0716fa1a27d66d75880be5f0
Mike Rotondo <mrotondo@gmail.com>
2026-02-20 14:16:46 -0800

    feat: Update the ALB SSL policy to the current default. (#47)
    Update the ALB SSL policy to the current default

--- alb.tf
+++ alb.tf
@@ -41,7 +41,7 @@ module "alb" {
     https = merge({
       port            = 443
       protocol        = "HTTPS"
-      ssl_policy      = "ELBSecurityPolicy-TLS-1-2-2017-01"
+      ssl_policy      = "ELBSecurityPolicy-TLS13-1-2-Res-PQ-2025-09"
       certificate_arn = aws_acm_certificate.endpoint["this"].arn
       forward = {
         target_group_key = "endpoint"
